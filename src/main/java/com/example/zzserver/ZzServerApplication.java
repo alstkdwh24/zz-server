556ade5ae71c2cd4af53763d2fdb0396a6f6237d
alstkdwh24 <alstkdwh24@naver.com>
2025-05-29 21:41:15 +0900

    h2적용

---
+++ src/main/java/com/example/zzserver/ZzServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.zzserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zzserver/ZzServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zzserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

46cd9597630b4d710f60a58427d5d2e7b6eac74a
keepConcentration <phm543@gmail.com>
2025-06-12 20:06:22 +0900

    숙소 조회 API 엔드포인트 수정 및 H2 콘솔 접근 설정 추가

--- src/main/java/com/example/zzserver/config/SecurityConfig.java
+++ src/main/java/com/example/zzserver/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,7 +26,7 @@ public class SecurityConfig {
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     private static final String[] AUTH_WHITELIST = {"**", "/member/login", "/member/signup",
-            "/swagger-ui/**", "/api-docs", "swagger-ui-custom.html"
+            "/swagger-ui/**", "/api-docs", "swagger-ui-custom.html", "/h2-console/**"
     };
 
     public SecurityConfig(CustomUserDetailsService customUserDetailsService, JwtUtil jwtUtil, CustomAccessDeniedHandler customAccessDeniedHandler, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
@@ -41,6 +42,11 @@ public class SecurityConfig {
 
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors((Customizer.withDefaults()));
+
+        // h2-console 에서 사용하는 X-FRAME-OPTIONS 허용
+        http.headers(headers -> headers
+            .frameOptions(FrameOptionsConfig::sameOrigin)
+        );
 
         //세션 관리 상태 없음으로 구성, Spring Security가 세션을 생성하지 않도록 설정
         http.sessionManagement(sessionMangement -> sessionMangement.sessionCreationPolicy(

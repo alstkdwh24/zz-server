51b35a09967eaa499e0a7ab3559a505c26c04957
User <alstkdwh24@naver.com>
2025-11-23 00:02:41 +0900

    accommodationDetail 페이지 오양은 다 갖춤

--- src/main/java/com/example/zzserver/accommodation/controller/AccommodationController.java
+++ src/main/java/com/example/zzserver/accommodation/controller/AccommodationController.java
@@ -1,12 +1,18 @@
 package com.example.zzserver.accommodation.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/list")
 public class AccommodationController {
+
+    @Value("${kakao.KakaoLoginJavaScriptKey}")
+    private String kakaoMap;
 
     @GetMapping("/accommodation/accommodationList")
     public String accommodationList(){
@@ -15,7 +21,9 @@ public class AccommodationController {
 
 
     @GetMapping("/accommodation/accommodationDetail")
-    public String accommodationDetail(){
+    public String accommodationDetail(Model model){
+        model.addAttribute("kakaoMap", kakaoMap);
+        System.out.println("kakaoMap" + kakaoMap);
         return "list/accommodation/accommodationDetail";
     }
 }

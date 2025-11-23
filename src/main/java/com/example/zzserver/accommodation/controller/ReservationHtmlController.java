8d10d54833cb0ba9167e189222e8bc730356475b
User <alstkdwh24@naver.com>
2025-11-23 20:26:07 +0900

    reservationCancel 페이지 제작 화면은 완성

---
+++ src/main/java/com/example/zzserver/accommodation/controller/ReservationHtmlController.java
@@ -0,0 +1,20 @@
+package com.example.zzserver.accommodation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reservation")
+public class ReservationHtmlController {
+
+    @GetMapping("/beforeReservation")
+    public String reservation(){
+        return "reservation/beforeReservation";
+    }
+
+    @GetMapping("/reservationCancel")
+    public String reservationCancel(){
+        return "reservation/reservationCancel";
+    }
+}
